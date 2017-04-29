c9ec9065fd3dda6c6f0101b8d43bad8fe7c1263c
kphloonafh <kphloonafh@gmail.com>
2017-04-29 17:32:52 +0000

    now Complex has kind * -> *

--- src/Data/Complex.purs
+++ src/Data/Complex.purs
@@ -2,80 +2,88 @@ module Data.Complex where
 
 import Math (Radians, cos, sin, sqrt)
 import Prelude
+import Data.Traversable (class Foldable, class Traversable, sequenceDefault, foldMapDefaultR)
 
-data Complex = Complex Number Number
+data Complex a = Complex a a
 
 type R = Number
-type C = Complex
-type Point = { x :: R, y :: R }
+type C = Complex Number
 
-infix 0 Complex as :!
+infix 0 Complex as :+
 
-conjugate :: C -> C
+conjugate :: ∀ a. Field a => Complex a -> Complex a
 conjugate (Complex r i) = Complex r (-i)
 
-scale :: R -> C -> C
+scale :: ∀ a. Field a => a -> Complex a -> Complex a
 scale a (Complex r i) = Complex (r*a) (i*a)
 
-mapR :: (R -> R) -> C -> C
+mapR :: ∀ a. Field a => (a -> a) -> Complex a -> Complex a   
 mapR f (Complex r i) = Complex (f r) i
-mapI :: (R -> R) -> C -> C
+
+mapI :: ∀ a. Field a => (a -> a) -> Complex a -> Complex a   
 mapI f (Complex r i) = Complex r (f i)
 
-real :: C -> R
+real :: ∀ a. Field a => Complex a -> a
 real (Complex r _) = r
 
-toImg :: R -> C
-toImg n = Complex zero n
-
-unitIm :: R -> C
-unitIm n = Complex one n
-
-toReal :: R -> C
-toReal n = Complex n zero
-
-image :: C -> R
+image :: ∀ a. Field a => Complex a -> a
 image (Complex _ i) = i
 
-length :: C -> R
-length (Complex r i) = sqrt (sqr r + sqr i) where sqr x = x * x
+toImg :: ∀ a. Field a => a -> Complex a
+toImg n = Complex zero n
 
+toReal :: ∀ a. Field a => a -> Complex a
+toReal n = Complex n zero
+
+{--abs :: ∀ a. Field a => Complex a -> a--}
+abs :: C -> R
+abs (Complex r i) = sqrt (sqr r + sqr i) where sqr x = x * x
+
+{--normalize :: ∀ a. Field a => Complex a -> Complex a--}
 normalize :: C -> C
-normalize z = l ^* z where l = one / length z
+normalize z = l ^* z where l = one / abs z
 
-angle :: Radians -> C
+angle :: Radians -> Complex Number
 angle θ = Complex (cos θ) (sin θ)
-
-toRecord :: C -> Point
-toRecord (Complex r i) = {x: r, y: i}
-
-fromRecord :: Point -> C
-fromRecord {x,y} = Complex x y
 
 infix 3 scale as ^*
 
-ap :: ∀ a. (R -> R -> a) -> C -> a
+ap :: ∀ a b. Field a => (a -> a -> b) -> Complex a -> b
 ap f (Complex r i) = f r i
 
-instance complexRing :: Ring Complex where
+instance complexRing :: Field a => Ring (Complex a) where
   sub (Complex r i) (Complex r' i') = Complex (r - r') (i - i')
 
-instance complexSemiring :: Semiring Complex where
+instance complexSemiring :: Field a => Semiring (Complex a) where
   one = Complex one zero
   mul (Complex r i) (Complex r' i') = Complex (r'* r - i'* i) (i'* r + r'* i)
 
   zero = Complex zero zero
   add (Complex r i) (Complex r' i') = Complex (r'+ r) (i'+ i)
 
-instance complexEuclideanRing :: EuclideanRing Complex where
+instance complexEuclideanRing :: Field a => EuclideanRing (Complex a) where
   mod (Complex r i) (Complex r' i') = 
     Complex ((r*r'+ i*i')/v) ((i*r'- r*i')/v) where v = (r*r + i'*i')
   div (Complex r i) (Complex r' i') = 
     Complex ((r*r'+ i*i')/v) ((i*r'- r*i')/v) where v = (r*r + i'*i')
   degree _ = 1
 
-instance complexCommutativeRing :: CommutativeRing Complex 
-instance complexField :: Field Complex 
+instance complexCommutativeRing :: Field a => CommutativeRing (Complex a)
+instance complexField :: Field a => Field (Complex a)
 
-instance complexShow :: Show Complex where
+instance complexFold :: Foldable Complex where
+  foldr f acc (Complex r i) = f r $ f i acc
+  foldl f acc (Complex r i) = f (f acc r) i
+  foldMap = foldMapDefaultR
+
+instance complexFunctor :: Functor Complex where
+  map f (Complex r i) = Complex (f r) (f i)
+
+instance complexTraversable :: Traversable Complex where
+  traverse f (Complex r i) = Complex <$> f r <*> f i
+  sequence = sequenceDefault
+
+instance complexShow :: Show a => Show (Complex a) where
   show (Complex r i) = show r <> " + " <> show i <> "i "
+
+csqrt 
